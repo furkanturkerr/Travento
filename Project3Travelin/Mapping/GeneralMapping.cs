8396eb3a4caa7ee6ea00387e33fd38448f550088
furkanturker <furkanturker.dev@gmail.com>
2026-02-17 15:53:15 +0300

    Ders 3

---
+++ Project3Travelin/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using Project3Travelin.Dtos.CategoryDtos;
+using Project3Travelin.Dtos.TourDtos;
+using Project3Travelin.Entities;
+
+namespace Project3Travelin.Mapping;
+
+public class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Category, ResultCategoryDto>().ReverseMap();
+        CreateMap<Category, CreateCategoryDto>().ReverseMap();
+        CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+        CreateMap<Category, GetCategoryByIdDto>().ReverseMap();
+        
+        CreateMap<Tour, GetTourByIdDto>().ReverseMap();
+        CreateMap<Tour, ResultTourDto>().ReverseMap();
+        CreateMap<Tour, CreateTourDto>().ReverseMap();
+        CreateMap<Tour, UpdateTourDto>().ReverseMap();
+    }
+}
